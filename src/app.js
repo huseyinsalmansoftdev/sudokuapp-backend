745a74bc9fc79d2e9d6b9e9a4ea9efa75cfd585f
huseyinsalmansoftdev <huseyinsalmansoftdev@gmail.com>
2023-12-17 17:38:37 -0800

    add build files

--- src/app.js
+++ src/app.js
@@ -1,11 +1,18 @@
 import express from 'express';
 import cors from 'cors';
+import { fileURLToPath } from 'url'; // Import the fileURLToPath function
+import path , { dirname } from 'path';
 import Router from './router/Router.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use(express.static(path.join(__dirname, 'build')));
 
 app.use('/', Router);
 
